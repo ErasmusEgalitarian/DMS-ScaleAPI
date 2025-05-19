096793eddc84a5bb20662a1570d029aeab7d739a
Draeby01 <carldraebypedersen@gmail.com>
2025-05-20 00:46:54 +0200

    Installed and updated some tools and packages to work with Avalonia UI and .NET 8.0. Implemented the ability to run the application in the browser.

--- AvaloniaAppUpdatedVersion/AvaloniaAppUpdatedVersion/App.axaml.cs
+++ AvaloniaAppUpdatedVersion/AvaloniaAppUpdatedVersion/App.axaml.cs
@@ -56,7 +56,7 @@ public partial class App : Application
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
 
-            singleViewPlatform.MainView = new MainView
+            singleViewPlatform.MainView = new SidebarMenuView
             {
                 DataContext = services.GetRequiredService<MainViewModel>()
             };
